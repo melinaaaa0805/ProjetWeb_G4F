21969d94f9030af99bf8e82c59bdc5a9224eaf80
Melina Mitterrand <melina.mitterrand08@gmail.com>
2022-12-15 15:25:13 +0100

    master

--- app/Views/v_menu.php
+++ app/Views/v_menu.php
@@ -13,7 +13,8 @@
     <link href="/public/assets/img/G4F.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,
+    400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS -->
     <link href="/public/assets/vendor/aos/aos.css" rel="stylesheet">
@@ -23,8 +24,12 @@
     <link href="/public/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="/public/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="/public/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/timecircles/1.5.3/TimeCircles.min.js" integrity="sha512-FofOhk0jW4BYQ6CFM9iJutqL2qLk6hjZ9YrS2/OnkqkD5V4HFnhTNIFSAhzP3x//AD5OzVMO8dayImv06fq0jA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59Hg
+    ZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"
+            referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/timecircles/1.5.3/TimeCircles.min.js"
+            integrity="sha512-FofOhk0jW4BYQ6CFM9iJutqL2qLk6hjZ9YrS2/OnkqkD5V4HFnhTNIFSAhzP3x//AD5OzVMO8dayImv06fq0jA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Template Main CSS-->
     <link href="/public/assets/css/style.css" rel="stylesheet">
@@ -43,7 +48,7 @@
                 <li><a class="nav-link scrollto active" href="#">Accueil</a></li>
                 <li><a class="nav-link scrollto" href="#">Section Cybersécurité</a></li>
                 <li><a class="nav-link scrollto" href="#">Section Cosplay</a></li>
-                <li class="dropdown"><a href="#"><span>Section tournois</span> <i class="bi bi-chevron-down"></i></a>
+                <li class="dropdown"><a href="#"><span>Section tournois</span> </a>
                     <ul>
                         <li><a href="#">Présentation</a></li>
                         <li><a href="#">Espace Nintendo</a></li>
