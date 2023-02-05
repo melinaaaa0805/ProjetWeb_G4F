38896914dc2a1bac76435405f2ccc7967891c528
Melina Mitterrand <melina.mitterrand08@gmail.com>
2023-02-05 21:58:00 +0100

    master

--- app/Views/v_inscriptionTournois.php
+++ app/Views/v_inscriptionTournois.php
@@ -1,1 +1,109 @@
-<?php
+<?php ?>
+<div>
+    <div>
+        <h1>Les Jeux Switch</h1>
+        <h4> <?php echo $jeux[0]; ?></h4>
+       <?php foreach ($horaire as $uneheure):
+       ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+    </div>
+    <div>
+        <h4> <?php echo $jeux[1]; ?></h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+    </div>
+    <div>
+        <h4>Le concours dépendra des votes</h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+    </div>
+    <div>
+        <h1>Les Jeux Playstation</h1>
+        <h4> <?php echo $jeux[0]; ?></h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+    </div>
+    <div>
+        <h4> <?php echo $jeux[0]; ?></h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+
+    </div>
+    <div>
+        <h4>Le concours dépendra des votes</h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+
+    </div>
+    <div>
+        <h1>Les Jeux Xbox</h1>
+        <h4> <?php echo $jeux[0]; ?></h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+
+    </div>
+    <div>
+        <h4> <?php echo $jeux[0]; ?></h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+
+    </div>
+    <div>
+        <h4>Le concours dépendra des votes</h4>
+        <?php foreach ($horaire as $uneheure):
+        ?>
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-success mx-auto fs-4 py-1 px-3" type="submit" name="<?php
+            $uneheure->IdConcours_avoirLieu?>" value="<?php
+            $uneheure->Date_avoirLieu ?>">
+        </div>
+        <?php endforeach;?>
+    </div>
+</div>
